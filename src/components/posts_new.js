763d0647bf4b254190887f442e14347e33e9d15c
liang <liang.su@beyondz.com.hk>
2018-01-30 13:59:23 +0800

    Install redux-form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form'
+// reduxForm like connect helper
 
+class PostsNew extends Component {
 
-export default class PostsNew extends Component {
+    renderTitleField(field) {
+        return (
+            <div>
+                <input 
+                    type="text"
+                    {...field.input}
+                />
+            </div>
+        );
+    }
 
     render() {
         return (
             <div>
-                Post new
+                <form>
+                    <Field
+                        name="title"
+                        component={this.renderTitleField}
+                    />
+                </form>
             </div>
         )
     }
 }
+
+export default  reduxForm({
+    form: 'PostsNewForm'
+})(PostsNew);
